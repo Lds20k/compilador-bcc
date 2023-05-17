8af9e66a903359b1d2f9572a072334aa020dc1a6
Lucas da Silva dos Santos <lucassilva.lds@outlook.com>
2023-05-17 18:18:10 -0300

    fix: repl

--- repl/main.c
+++ repl/main.c
@@ -19,87 +19,177 @@
 #define INTEGER_NUMBER "INTEGER_NUMBER\0"
 #define REAL_NUMBER "REAL_NUMBER\0"
 
-typedef struct token_s
-{
+bool operation_erro = false;
+
+typedef struct token_s{
     char* name;
     char* characters;
 } token_t;
 
-typedef struct value_s
-{
+typedef union number_u{
     int integer_value;
     float real_value;
+} number_t;
+
+typedef struct value_s{
+    bool real;
+    number_t number;
 } value_t;
+
+bool is_operation(const char* character){
+    return !strcmp(character, SUM) ||
+           !strcmp(character, SUBTRACTION) ||
+           !strcmp(character, MULTIPLICATION) ||
+           !strcmp(character, DIVISION);
+}
 
 bool is_number(const char* character){
     return *character > 47 && *character < 58;
 }
 
-value_t parse_number(list_t* tokens, int index){
+value_t number_sum(value_t a, value_t b){
     value_t value = {0};
-    if(tokens->length >= 1){
-        token_t* token = (token_t*) get_from_list(tokens, index)->data;
 
-        if (strcmp(token->name, INTEGER_NUMBER) != 0 && strcmp(token->name, REAL_NUMBER) != 0){
-            printf("Unexpected token %s of value %s\n", token->name, token->characters);
-            value_t value = {0};
-            return value;
-        }
-        
-        if(strcmp(token->name, INTEGER_NUMBER) == 0)
-            value.integer_value = strtol(token->characters, NULL, 10);
+    if(a.real && b.real){
+        value.real = true;
+        value.number.real_value = a.number.real_value + b.number.real_value;
+    }else if (a.real || b.real){
+        value.real = true;
+        if(a.real)
+            value.number.real_value = a.number.real_value + (float) b.number.integer_value;
+        else
+            value.number.real_value = (float) a.number.integer_value + b.number.real_value;
+    }else{
+        value.real = false;
+        value.number.integer_value = a.number.integer_value + b.number.integer_value;
+    }
 
-        if(strcmp(token->name, REAL_NUMBER) == 0)
-            value.real_value = strtof(token->characters, NULL);
+    return value;    
+}
+
+value_t number_subtraction(value_t a, value_t b){
+    value_t value = {0};
+
+    if(a.real && b.real){
+        value.real = true;
+        value.number.real_value = a.number.real_value - b.number.real_value;
+    }else if (a.real || b.real){
+        value.real = true;
+        if(a.real)
+            value.number.real_value = a.number.real_value - (float) b.number.integer_value;
+        else
+            value.number.real_value = (float) a.number.integer_value - b.number.real_value;
+    }else{
+        value.real = false;
+        value.number.integer_value = a.number.integer_value - b.number.integer_value;
+    }
+
+    return value;    
+}
+
+value_t number_multiplication(value_t a, value_t b){
+    value_t value = {0};
+
+    if(a.real && b.real){
+        value.real = true;
+        value.number.real_value = a.number.real_value * b.number.real_value;
+    }else if (a.real || b.real){
+        value.real = true;
+        if(a.real)
+            value.number.real_value = a.number.real_value * (float) b.number.integer_value;
+        else
+            value.number.real_value = (float) a.number.integer_value * b.number.real_value;
+    }else{
+        value.real = false;
+        value.number.integer_value = a.number.integer_value * b.number.integer_value;
+    }
+
+    return value;    
+}
+
+value_t number_division(value_t a, value_t b){
+    value_t value = {0};
+
+    if(a.real && b.real){
+        value.real = true;
+        value.number.real_value = a.number.real_value / b.number.real_value;
+    }else if (a.real || b.real){
+        value.real = true;
+        if(a.real)
+            value.number.real_value = a.number.real_value / (float) b.number.integer_value;
+        else
+            value.number.real_value = (float) a.number.integer_value / b.number.real_value;
+    }else{
+        value.real = true;
+        value.number.real_value = (float) a.number.integer_value / (float) b.number.integer_value;
+    }
+
+    return value;    
+}
+
+value_t parse_number(token_t* token){
+    value_t value = {0};
+    
+    if(strcmp(token->name, INTEGER_NUMBER) == 0){
+        value.number.integer_value = strtol(token->characters, NULL, 10);
+        value.real = false;
+    }
+
+    if(strcmp(token->name, REAL_NUMBER) == 0){
+        value.number.real_value = strtof(token->characters, NULL);
+        value.real = true;
     }
 
     return value;
 }
 
-value_t parse_term(list_t* tokens, size_t index, bool operation_flag, bool number_flag){
-    if(index >= tokens->length){
-        if(number_flag){
-            printf("Invalid operation token syntax\n");
-            value_t value = {0};
-            return value;
-        }
-        
-        value_t value = {0};
-        return value;
+value_t parse_term(list_t* tokens, size_t index){
+    size_t current_index = index;
+    token_t* token = (token_t*) get_from_list(tokens, current_index)->data;
+    while (!strcmp(token->name, SPACE)){
+        current_index++;
+        token = (token_t*) get_from_list(tokens, current_index)->data;
     }
 
-    token_t* token = (token_t*) get_from_list(tokens, index)->data;
+    if (strcmp(token->name, INTEGER_NUMBER) && strcmp(token->name, REAL_NUMBER)){
+        printf("Unexpected token %s of value \"%s\" in column %d, number token is expected\n", token->name, token->characters, current_index + 1);
+        operation_erro = true;
+        return (value_t){0};
+    }
 
-    if(strcmp(token->name, SPACE) == 0) return parse_term(tokens, index + 1, operation_flag, number_flag);
+    current_index++;
+    data_t* data = get_from_list(tokens, current_index);
+    if (data == NULL) return parse_number(token);
+
+    token_t* next_token = (token_t*) data->data;    
+    while (!strcmp(next_token->name, SPACE)){
+        current_index++;
+        next_token = (token_t*) get_from_list(tokens, current_index)->data;
+    }
+
+    value_t number = parse_number(token);
+    value_t next_number = parse_term(tokens, current_index + 1);
+
+    
+
+    if(!strcmp(next_token->name, SUM))
+        return number_sum(number, next_number);
+    else if(!strcmp(next_token->name, SUBTRACTION))
+        return number_subtraction(number, next_number);
+    else if(!strcmp(next_token->name, MULTIPLICATION))
+        return number_multiplication(number, next_number);
+    else if(!strcmp(next_token->name, DIVISION)){
+        if (!next_number.number.integer_value || !next_number.number.real_value){
+            printf("Division by zero is not a valid operation\n");
+            operation_erro = true;
+            return (value_t){0};
+        }
+        return number_division(number, next_number);
+    }
     else {
-        if(!strcmp(token->name, SUM) && operation_flag)
-            return parse_term(tokens, index + 1, false, true);
-        if(!strcmp(token->name, SUBTRACTION) && operation_flag)
-            return parse_term(tokens, index + 1, false, true);
-        else if(operation_flag){
-            printf("Unexpected token %s of value %s\n", token->name, token->characters);
-            value_t value = {0};
-            return value;
-        }
-
-        value_t return_value = {0};
-        value_t value = parse_number(tokens, index);
-        value_t next_value = parse_term(tokens, index + 1, true, false);
-
-        bool value_real = value.real_value != 0 && value.integer_value == 0;
-        bool next_value_real = next_value.real_value != 0 && next_value.integer_value == 0;
-
-        if (value_real || next_value_real){
-            return_value.real_value = (value_real) ? value.real_value : (float)value.integer_value;
-            return_value.real_value += (next_value_real) ? next_value.real_value : (float)next_value.integer_value;
-
-            return return_value;
-        }
-        
-        return_value.integer_value = value.integer_value;
-        return_value.integer_value += next_value.integer_value;
-
-        return return_value;
+        printf("Unexpected token %s of value \"%s\" in column %d, operation token is expected\n", next_token->name, next_token->characters, current_index + 1);
+        operation_erro = true;
+        return (value_t){0};
     }
 }
 
@@ -107,7 +197,7 @@ value_t parse_expression(list_t* list){
     value_t value = {0};
     if (list->length < 1) return value;
 
-    return parse_term(list, 0, false, false);
+    return parse_term(list, 0);
 }
 
 list_t* tokenization(char* string){
@@ -182,7 +272,7 @@ int main(int argc, char const *argv[]){
     bool quit = false;
 
     while(!quit){
-        printf(">>");
+        printf(">> ");
         fgets(data, 256, stdin);
         data[strcspn(data, "\n")] = 0;
         if(!strcmp(data, "quit")){
@@ -196,10 +286,13 @@ int main(int argc, char const *argv[]){
         
         value_t value = parse_expression(tokens);
 
-        if (value.real_value != 0 && value.integer_value == 0)
-            printf("%f\n", value.real_value);
+        if(!operation_erro)
+            if (value.real)
+                printf("%f\n", value.number.real_value);
+            else
+                printf("%d\n", value.number.integer_value);
         else
-            printf("%d\n", value.integer_value);
+            operation_erro = false;
 
         for(size_t i = 0; i < tokens->length; i++){
             token_t* token = (token_t*) get_from_list(tokens, i)->data;
